83904bbe7a59fb048484d996e1b7a377ae8beb63
Peter Morihladko <morihladko@gmail.com>
2017-02-25 02:20:28 +0100

    Change of celery outside worker
    I removed the celery app initialization from the "worker" to the
    application itself.

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,10 +1,12 @@
+from celery import Celery
+
 from zsl.interface.importer import initialize_web_application
 initialize_web_application()
 
 from zsl.application.service_application import service_application
 
-from zsl.interface.celery.worker import CeleryTaskQueueOutsideWorkerModule, CeleryTaskQueueWorkerBase
+from zsl.interface.celery.worker import CeleryTaskQueueOutsideWorkerModule
 
 service_application.add_injector_module(CeleryTaskQueueOutsideWorkerModule)
 
-app = service_application.get_injector().get(CeleryTaskQueueWorkerBase).celery_app
+app = Celery(backend='rpc', broker='redis://localhost')
